263d0d70986ba9e6d08201d3fece8c5dbee7ad8a
Anjan Rai <rai.unknown@gmail.com>
2022-10-02 21:52:12 +1100

    added table number and order type fields in the order table

--- app/models/order.model.js
+++ app/models/order.model.js
@@ -1,22 +1,35 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize, Sequelize) => {
-  const Order = sequelize.define('orders', {
-    id: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      primaryKey: true,
+  const Order = sequelize.define(
+    'orders',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        unique: true,
+        primaryKey: true,
+      },
+      customerName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      orderType: {
+        type: DataTypes.STRING,
+        values: ['Dine In', 'Take Away'],
+        validate: {
+          customValidator(value) {
+            if (['Dine In', 'Take Away'].indexOf(value) === -1) {
+              throw new Error('Invalid order type.');
+            }
+          },
+        },
+      },
+      tableNumber: {
+        type: DataTypes.INTEGER,
+      },
     },
-    lastUpdated: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-    customerName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    { timestamps: true }
+  );
   Order.sync();
   return Order;
 };
